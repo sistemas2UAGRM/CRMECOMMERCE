b97d26155516ff09bbd6be09beafcdedf3ee8dea
Jorge Adhemar Alba Copa <albacopaj@gmail.com>
2025-10-11 16:41:43 -0400

    eliminacion de errores

--- Backend/api/v1/crm/services/__init__.py
+++
@@ -1,7 +0,0 @@
-# api/v1/crm/services/__init__.py
-
-from .role_service import RoleService
-from .permission_service import PermissionService
-from .user_role_service import UserRoleService
-
-__all__ = ['RoleService', 'PermissionService', 'UserRoleService']
--- Backend/api/v1/users/services/__init__.py
+++
@@ -1,6 +0,0 @@
-# api/v1/users/services/__init__.py
-
-from .auth_service import AuthService
-from .user_management_service import UserManagementService
-
-__all__ = ['AuthService', 'UserManagementService']
